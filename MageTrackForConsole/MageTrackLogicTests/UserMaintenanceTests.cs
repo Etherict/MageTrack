a93766ccecfc64f5fa040efa9cb9a2003523db39
Etherict <etherict@etherictandidm.net>
2014-05-13 16:10:53 -0500

    Minor updates
    Minor updates

--- MageTrackForConsole/MageTrackLogicTests/UserMaintenanceTests.cs
+++ MageTrackForConsole/MageTrackLogicTests/UserMaintenanceTests.cs
@@ -9,31 +9,5 @@ namespace MageTrackLogicTests
         string plainTextPassword = "password";
         string incorrectPlainTextPassword = "notPassword";
         string userName = "user";
-        int workForce = 10;
-
-        [TestMethod]
-        public void EncryptPassword_EncryptsPassword()
-        {
-            MageTrackLogic.User user = new MageTrackLogic.User();
-            user.UserName = userName;
-            user.PlainTextPassword = plainTextPassword;
-            user.IsNewUser = true;
-            new MageTrackLogic.UserMaintenance().EncryptPassword(user);
-            Assert.AreNotEqual(user.HashedPassword, user.PlainTextPassword);
-        }
-
-        [TestMethod]
-        public void EncryptPassword_RunsForSufficientTime()
-        {
-            MageTrackLogic.User user = new MageTrackLogic.User();
-            user.UserName = userName;
-            user.PlainTextPassword = plainTextPassword;
-            user.IsNewUser = true;
-            System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
-            timer.Start();
-            new MageTrackLogic.UserMaintenance().EncryptPassword(user);
-            timer.Stop();
-            Assert.IsTrue(timer.ElapsedMilliseconds >= 80);
-        }
     }
 }
